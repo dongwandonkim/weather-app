acc046acce3b2f903817fb9f88df90a1bfbb79ee
Dongwan Kim <dongwan.don.kim@gmail.com>
2020-06-04 21:44:43 -0700

    init commit

---
+++ src/app.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars template engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: "Don's weather app", name: 'don kim' });
+});
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About', name: 'don kim' });
+});
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'don kim',
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'you must provide an address',
+    });
+  }
+  geocode(req.query.address, (err, { longitude, latitude, location } = {}) => {
+    if (err) {
+      return res.send({ err });
+    }
+    forecast(longitude, latitude, (err, forecastData) => {
+      if (err) {
+        return res.send({ err });
+      }
+      res.send({
+        address: req.query.address,
+        location,
+        forecast: forecastData,
+      });
+    });
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'don kim',
+    message: 'Help article not found',
+  });
+});
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'don kim',
+    message: 'Page not found',
+  });
+});
+app.listen(3000, () => {
+  console.log(' server is running on port:3000');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+  const url =
+    'http://api.weatherstack.com/current?access_key=060ca68507c9fe216c88b42d587435d9&query=' +
+    latitude +
+    ',' +
+    longitude;
+
+  request({ url, json: true }, (err, { body }) => {
+    if (err) {
+      callback('Unable to connect to weather service', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      //console.log(body.current);
+      const data = `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. There is ${body.current.precip}% chance of rain.`;
+      callback(undefined, data);
+    }
+  });
+};
+
+module.exports = forecast;
